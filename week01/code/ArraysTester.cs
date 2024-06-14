de5048581c8946db8cb6ddc96e5a89a23990dea1
ellachamb <ellachamb@gmail.com>
2024-06-14 15:34:07 -0600

    week01

--- week01/code/ArraysTester.cs
+++ week01/code/ArraysTester.cs
@@ -34,12 +34,15 @@ public static class ArraysTester {
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     private static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
-
-        return new double[0]; // replace this return statement with your own
+        // Initialize the array that is the size of int length
+        // Use a for loop to calculate the multiples of the number and store them in the array
+        // Return the array
+        double[] multiples = new double[length];
+        for (int i = 0; i < length; i++)
+        {
+            multiples[i] = number * (i + 1);
+        }
+        return multiples;
     }
     
     /// <summary>
@@ -52,10 +55,13 @@ public static class ArraysTester {
     /// </summary>
     private static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
-
+        // take modulus of amount and length of data and assign it to rotation
+        // 
+        int rotation = amount % data.Count;
+        List<int> rotatedList = new List<int>();
+        rotatedList.AddRange(data.GetRange(data.Count - rotation, rotation));
+        rotatedList.AddRange(data.GetRange(0, data.Count - rotation));
+        data.Clear();
+        data.AddRange(rotatedList);
     }
 }
